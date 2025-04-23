a65853d0d331a1a83f62832fbe46884a14c57df5
Vule <lqvu@tma.com.vn>
2025-04-23 20:09:53 +0700

    update modules

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -1,11 +1,5 @@
 # modules/infrastructure/main.tf
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
-
 # Create Resource Group
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
