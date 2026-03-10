2324504ed514808138c47671382864c5b89b07f6
lqvu2025 <lqvu@tma.com.vn>
2026-03-10 14:05:25 +0700

    fix: comment out core_resources outputs in outputs.tf

--- env/prod/outputs.tf
+++ env/prod/outputs.tf
@@ -1,3 +1,5 @@
+# outputs.tf - only active outputs that don't reference commented-out modules
+
 output "api_gateway_urls" {
   description = "Map of API Gateway URLs for each API"
   value = {
@@ -12,52 +14,6 @@ output "stage_names" {
   }
 }
 
-# output "rds_writer_endpoint" {
-#   description = "RDS writer endpoint"
-#   value       = module.core_resources.rds_writer_endpoint
-# }
-
-# output "rds_cluster_endpoint" {
-#   description = "RDS cluster endpoint"
-#   value       = module.core_resources.rds_cluster_endpoint
-# }
-
-# output "cloudfront_distributions" {
-#   description = "CloudFront distribution URLs"
-#   value = {
-#     for key, api in module.rest_api_gw : key => api.stage_name
-#   }
-# }
-
-
-
-
-
-# # output "api_gateway_url" {
-# #   description = "API Gateway invocation URL"
-# #   value       = module.rest_api_gw.api_gateway_url
-# # }
-
-# # output "stage_name" {
-# #   description = "API Gateway stage name"
-# #   value       = module.rest_api_gw.stage_name
-# # }
-
-# Expose module outputs for debugging
-output "rds_cluster_endpoint" {
-  description = "RDS cluster endpoint"
-  value       = module.core_resources.rds_cluster_endpoint
-}
-
-output "rds_endpoints" {
-  description = "All RDS instance endpoints"
-  value       = module.core_resources.rds_endpoints
-}
-
-output "rds_reader_endpoint" {
-  description = "RDS reader endpoint"
-  value       = length(module.core_resources.rds_reader_endpoint) > 0 ? module.core_resources.rds_reader_endpoint[0] : null
-}
 output "websocket_api_names" {
   description = "Map of WebSocket API names for each WebSocket API"
   value = {
@@ -72,3 +28,7 @@ output "websocket_stage_names" {
   }
 }
 
+# Commented out - module.core_resources is disabled for 2-lambda test
+# output "rds_cluster_endpoint" { ... }
+# output "rds_endpoints" { ... }
+# output "rds_reader_endpoint" { ... }
